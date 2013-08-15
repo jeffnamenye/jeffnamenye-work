ea5500806a161f5f9f01d6c8d63d2173a51c7242
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-14 21:06:19 -0400

    changes to script

--- Week2/Expressions/Expressions_Wacky/js/script.js
+++ Week2/Expressions/Expressions_Wacky/js/script.js
@@ -7,13 +7,13 @@
 var manykids = prompt("Enter how many kids will attend.");// kids attend
 var moneySpent = prompt("How much money do you want to spend.");//money to spend
 var balloonKid = prompt("How many balloons per kid");//balloons per kid
-var costBallon = .99
+var costBalloon = .99
 
 var balloonTotal = manykids * balloonKid;// total of ballons needed
 var balloonCost = costBalloon * balloonTotal; //total cost of balloons
 var moneyLeft = moneySpent - balloonCost; //money left
 
-var balloonMoney = "This is how much for all the balloons"+ baloonCost// this how much it will cost for the balloons
+var balloonMoney = "This is how much for all the balloons"+ baloonCost;// this how much it will cost for the balloons
 alert(balloonMoney);//print out
 
 var moneyRemain = "This how much you will have left after cost of balloons" +moneyLeft;// this how much you will have left
