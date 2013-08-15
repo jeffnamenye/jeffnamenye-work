01740421ce2659af9ebcb58e2d8737b8b85599e3
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-15 18:19:32 -0400

    work done industry

--- Week2/Expressions/Expressions_Industry/js/script.js
+++ Week2/Expressions/Expressions_Industry/js/script.js
@@ -2,4 +2,9 @@
 // I am doing my proffesional on ohms law which I use almost everyday.
 
 
-var formulasV = prompt ("To figure out the voltage , current, or resistance of something there are bunch  of  different formulas. Here we will lfigure out current.  Output voltage, Current, and resistance.");           
+var formulasV = prompt ("To figure out the voltage , current, or resistance of something there are bunch  of  different formulas. Here we will lfigure out current.  Output voltage, Current, and resistance."); //explains what i am tring to do        
+
+var current prompt = "Please enter a value for current";
+var volts prompt = "Please enter a value for volts" ; 
+var resistance prompt = "Please enter a value for resistance";
+
