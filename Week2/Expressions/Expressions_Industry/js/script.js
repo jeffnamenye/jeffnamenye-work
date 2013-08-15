306074c4574324715ea650579642ca53b288e490
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-15 18:14:32 -0400

    first test

--- Week2/Expressions/Expressions_Industry/js/script.js
+++ Week2/Expressions/Expressions_Industry/js/script.js
@@ -1,2 +1,5 @@
 //Jeff Namenye 8/14/13 Expressions_Proffesional
-// I am doing it on ohms law which I use almost everyday.
+// I am doing my proffesional on ohms law which I use almost everyday.
+
+
+var formulasV = prompt ("To figure out the voltage , current, or resistance of something there are bunch  of  different formulas. Here we will lfigure out current.  Output voltage, Current, and resistance.");           
