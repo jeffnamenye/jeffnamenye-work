61f972d7618d5d560dff8b2e2eec05769c8be7a5
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-14 20:33:39 -0400

    Prompt amd alert work

--- Week2/Prompt and Alert/js/script.js
+++ Week2/Prompt and Alert/js/script.js
@@ -1,6 +1,6 @@
 // Jeff Namenye 8/13/13 EPrompts and Alerts
 
-var width = prompt("We are caculating the area of a rectangle. \nvaPlease eneter the width");
+var width = prompt("We are caculating the area of a rectangle. \nPlease eneter the width");
 var height = prompt("Please enter the height"); 
 var area = width * height;//caculate with that info
 //console.log(area);//prints it out to console
