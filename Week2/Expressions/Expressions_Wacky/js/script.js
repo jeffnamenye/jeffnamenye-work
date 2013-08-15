b75e04c12e963989d299a0e2908ab421aa0d4ead
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-14 20:46:32 -0400

    work on wacky

---
+++ Week2/Expressions/Expressions_Wacky/js/script.js
@@ -0,0 +1,12 @@
+// Jeff Namenye 8/14/13 Wacky_Expresions
+
+// Bucky the clown and balloon animals 
+
+// This is where the the user tells how many kids, how much money they want to spend, and how many baloons per kid.
+
+var manykids = prompt("Enter how many kids will attend.");// kids attend
+var moneySpent = prompt("How much money do you want to spend.");//money to spend
+var balloonKid = prompt("How many balloons per kid");//balloons per kid
+var costBallon =
+
+var balloonTotal = manykids * balloonsKid;// total of ballons needed
