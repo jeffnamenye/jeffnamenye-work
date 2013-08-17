44fb38c0acba6a34ca254f894508d8fbe51cf8d9
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-17 17:20:43 -0400

    added more code

---
+++ Week 3/Conditionals/Condition with Expression/js/script.js
@@ -0,0 +1,14 @@
+//Conditional logic-With an expression
+
+
+var kidHeight = 50;
+var miniHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print counsole "You can ride"
+//if the child is over 48 inches in height
+if (kidHeight + sneakerLift){
+	//counsel performed if conditions is true
+	console.log("You can ride");
+	
+}
