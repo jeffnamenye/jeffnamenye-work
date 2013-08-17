db919b0709f83d6cc056669b513c059171fdbb80
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-17 17:14:43 -0400

    Test code

---
+++ Week 3/Conditionals/Relational Expressions/js/script.js
@@ -0,0 +1,12 @@
+// Conditional logic-relational expressions
+
+var kidHeight = 50;
+
+var miniHeight = 48
+//if the child is old enough, print counsole "You can ride"
+//if the child is over 48 inches in height
+if (kidHeight > miniHeight){
+	//counsel performed if conditions is true
+	console.log("You can ride");
+	
+}
