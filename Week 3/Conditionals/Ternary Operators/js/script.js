0e379c6cd98693666113c733a3a1ffdf494e6e09
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-18 11:28:14 -0400

    worked on assignments

---
+++ Week 3/Conditionals/Ternary Operators/js/script.js
@@ -0,0 +1,8 @@
+//conditional logic- Ternary Operators
+
+var age = 6;
+var book;
+
+//If the child is under 10, the get Green Eggs and Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs And Ham" : "The Time Machine";
+console.log(book);
