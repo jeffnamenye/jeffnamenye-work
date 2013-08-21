313fd90d485044b4f93fff5efdc056e0e8dec316
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-21 18:54:04 -0400

    work on wacky

--- Week 3/Conditionals/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals/Conditionals_Wacky/js/script.js
@@ -1,1 +1,10 @@
 //Jeff Namenye 8/21/2013 Conditionals_Wacky
+
+//Enough air to reach lander
+//Caculate enough air to go from rover to the space capsule on moon
+//or die
+
+
+var air = 20; // air the tank hold in cubic feet
+var amountAir =
+
