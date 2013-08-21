36b75462e5a580b103bbc9045e8c57f2a16a8f8e
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-21 18:11:03 -0400

    Practice update

--- Week 3/Conditionals/Conditional Logic Concepts/js/script.js
+++
@@ -1,7 +0,0 @@
-//Jeff Namenye 8/17/2013 Conditional logic concepts
-
-var weather = prompt("Is the weather 80 degrees?");
-var goToTheBeach ="";
-var goToTheMovie = "";
-if (weather > 80) {goToTheBeach = "Go to the beach"; alert (goToTheBeach);}
-else{goToTheMovie = "Go to the movie"; alert(goToTheMovie);}
--- Week 3/Conditionals/Logic Concepts/js/script.js
+++
@@ -1,8 +0,0 @@
-
-var temp = prompt ("Enter the current tempature");
-var goToTheBeach ="";
-if(temp > 70) 
-{goToTheBeach = "Go to the beach";
-
-alert (goToTheBeach);
-}
