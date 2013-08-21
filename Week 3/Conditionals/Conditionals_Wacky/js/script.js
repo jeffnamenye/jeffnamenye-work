8711cba76df575bcc3080a562dcc41c65a32f5e2
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-21 19:18:50 -0400

    test wacky

--- Week 3/Conditionals/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals/Conditionals_Wacky/js/script.js
@@ -1,17 +1,17 @@
 //Jeff Namenye 8/21/2013 Conditionals_Wacky
 
 //Enough air to reach lander
-//Caculate enough air to go from rover to the space capsule 50 miles away on moon
+//Caculate enough air to go from rover to the space capsule 150 miles away on moon
 //or die
 
-
-var air = 20; // air the tank hold in cubic feet
+var speed = prompt ("Please enter how fast rover is going"); // this is the speed in mph
+var air = prompt ("How much air the tank holds"); // air the tank hold in cubic feet
 var amountAir = 10; //this is the amount you breath per hour
-var speed = 15; // this is the speed in mph
-var distance = 50; // distance to lander from rover
+
+var distance = 150; // distance to lander from rover
 
 //calculations
 var hoursRover = distance / speed;
 var timeReturn = air / amountAir;
  
-if (hoursRover > timeReturn)
+if (hoursRover > timeReturn) ? prompt  ("You die") : prompt ("You live") ; 
