46a9b408448ab9bacb53e26884987620291209d7
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-21 20:15:38 -0400

    work on Industry

--- Week 3/Conditionals/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals/Conditionals_Industry/js/script.js
@@ -1,1 +1,10 @@
 //Jeff Namenye 8/21/2013 Conditionals_Industry
+
+//Starting the linac laser
+//This what is takes to start the linac laser at work
+
+var amps = prompt ("Please enter the amount of amps being used");
+var volts = prompt ("Please enter the amount of votls being used");
+var doorsClosed = ("Please enter how many doors to vault are closed 1-3");
+
+}
