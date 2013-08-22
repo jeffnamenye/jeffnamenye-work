f8975d248f535cbd89d6290717855dafa3ad75ac
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-22 18:33:18 -0400

    add to code

--- Week 3/Conditionals/Conditonals_Personal/js/script.js
+++ Week 3/Conditionals/Conditonals_Personal/js/script.js
@@ -5,5 +5,10 @@
  
 
 var guest = prompt ("Please enter the number of guest");//this is the number of guest you have
-var budget = prompt ("Please enter your budget")
-var pricePlate = 10
+var budget = prompt ("Please enter your budget");//this id where you enter the aount you want to spend
+var pricePlate = 10 ; //this is thew price per plate
+
+var totalPrice = pricePlate * guest ; // this is where it gives you the total price
+var finalBudget = budget - totalPrice ;
+
+
