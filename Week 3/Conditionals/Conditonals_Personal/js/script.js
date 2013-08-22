0dc932586d001a3fef683e32dcc2693cca53c03d
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-22 18:09:58 -0400

    first for personal

--- Week 3/Conditionals/Conditonals_Personal/js/script.js
+++ Week 3/Conditionals/Conditonals_Personal/js/script.js
@@ -1,1 +1,4 @@
-//Jeff Namenye 8/21/2013 Conditionals_Industry
+//Jeff Namenye 8/21/2013 Conditionals_Personal
+
+//food budget
+//your having a party and what it catered in, this is to determine if your budget is enough.
