8a178bd64b1b8bcaddac1a317b1e260b298ff68e
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-22 18:14:43 -0400

    added code

--- Week 3/Conditionals/Conditonals_Personal/js/script.js
+++ Week 3/Conditionals/Conditonals_Personal/js/script.js
@@ -2,3 +2,8 @@
 
 //food budget
 //your having a party and what it catered in, this is to determine if your budget is enough.
+ 
+
+var guest = prompt ("Please enter the number of guest");//this is the number of guest you have
+var budget = prompt ("Please enter your budget")
+var pricePlate = 10
