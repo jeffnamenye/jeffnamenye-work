46c80f691c876066126422390aac8ffcc2e2f6e9
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-21 20:46:56 -0400

    test industry

--- Week 3/Conditionals/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals/Conditionals_Industry/js/script.js
@@ -11,10 +11,12 @@ var doorsClosed = prompt ("Please enter how many doors to vault are closed 1-3")
 var watts = amps * volts;// this where you multiply amps times volts to get you wattage
 
 
- (doorsClosed == 3) ? prompt ("All doors closed to run") : prompt ("You can not run, all doors not closed");
+ (doorsClosed == 3) ? prompt ("All doors closed to run") : prompt ("You can not run, all doors not closed");//Print out come
 
  if (watts >= 30000){
-	prompt ("You have enough voltage to run") ;}//this is where the out come of voltage
+	prompt ("You have enough wattage to run") ;}//this is where the out come of voltage
+} else { prompt ("You do not have enough wattage to run");
 
+}
 
 
