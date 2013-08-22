311ff26e735009fa3ad5d091bc38e2d0feb5b4a5
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-21 20:39:46 -0400

    work on industry

--- Week 3/Conditionals/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals/Conditionals_Industry/js/script.js
@@ -5,6 +5,16 @@
 
 var amps = prompt ("Please enter the amount of amps being used");// this is where you enter the amps
 var volts = prompt ("Please enter the amount of votls being used");//this is where you enter the volts
-var doorsClosed = ("Please enter how many doors to vault are closed 1-3");
+var doorsClosed = ("Please enter how many doors to vault are closed 1-3");//this is where you enter the number of doors you close
+
+
+var watts = amps * volts;// this where you multiply amps times volts to get you wattage
+
+
+ (doorsClosed == 3) ? prompt ("All doors closed to run") : prompt ("You can not run, all doors not closed");
+
+ if (watts >= 30000){
+	prompt ("You have enough voltage to run") ;}//this is where the out come of voltage
+
 
 
