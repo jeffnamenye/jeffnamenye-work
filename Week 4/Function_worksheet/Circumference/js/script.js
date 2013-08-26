12ffdac34ef13f6d8c819ed4cda7473480cb19f3
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-26 19:32:55 -0400

    test

--- Week 4/Function_worksheet/Circumference/js/script.js
+++ Week 4/Function_worksheet/Circumference/js/script.js
@@ -3,7 +3,7 @@
 
 // calaculate the circumference of a circle
 
-var circum = circRadius ( 4, 3.14);// argument 
+var circum = circRadius ( 10, 3.14);// argument 
 
 function circRadius (r, p){//Paramaters
 	var circumference = (2 * r * p);
