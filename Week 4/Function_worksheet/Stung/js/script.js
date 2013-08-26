5f653b3a76ca78ba36b42aecc166515c1f1a5dca
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-26 19:56:25 -0400

    code

--- Week 4/Function_worksheet/Stung/js/script.js
+++ Week 4/Function_worksheet/Stung/js/script.js
@@ -7,7 +7,8 @@ var kill = poundsKill (20, 8.666666667);//argument variables
 
 
 function poundsKill (lb, sting){//parameter vriables
-	var totalStings ( lb * sting);
+	var totalStings ( lb * sting);// calaculating the equatuion
+	var pounds = (lb;)// in pounds
 }
 
 
