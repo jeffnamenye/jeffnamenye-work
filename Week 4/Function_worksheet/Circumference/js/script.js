83347c417877762c593f89450c6544f18c2dfb5f
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-26 18:14:58 -0400

    homework

---
+++ Week 4/Function_worksheet/Circumference/js/script.js
@@ -0,0 +1,11 @@
+//Circumferece
+
+
+var calcArea = function(width * height){
+	//code the function runs
+	var area = width * height;
+	return area;
+}
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
---
+++ Week 4/Function_worksheet/Stung/js/script.js
@@ -0,0 +1,11 @@
+//Anoymous Functions
+
+
+var calcArea = function(width * height){
+	//code the function runs
+	var area = width * height;
+	return area;
+}
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
