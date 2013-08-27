cc5f20d7d185656f177c084c9f1144b579b89dae
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-27 19:32:27 -0400

    code and commets

--- Week 4/Assignment_Functions/Functions_Wacky/js/script.js
+++ Week 4/Assignment_Functions/Functions_Wacky/js/script.js
@@ -5,8 +5,11 @@
 
 var tank = prompt ("How much fuel do you use in lift off in a percent using a decimal")//prompt for gas use
 var fuel = 10000//fuel in tank
+var liftGas = 5000//how much gas is needed for lift off
+var totalGas = tank * fuel//claculation for fuel
+var gas;
 
-
+gas = ()
 
 
 var countDown = prompt ("Please tell the number between 1 and 10");//prompt for # for count down
