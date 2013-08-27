1908bd6347ba96a8eb3c6e75ba9684f0afeb304e
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-27 19:03:06 -0400

    code

--- Week 4/Assignment_Functions/Functions_Wacky/js/script.js
+++ Week 4/Assignment_Functions/Functions_Wacky/js/script.js
@@ -1,3 +1,6 @@
 // Jeff Namenye 8/27/13 Functions_Wacky
 
 
+//count down to launch
+
+var countDown = prompt ("Please tell the number you want to start from for count down");//prompt for # for count down
