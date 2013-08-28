d3fcbb05d3dab00a4549683b9d725644ae8bf357
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-28 19:26:08 -0400

    commet

--- Week 4/Assignment_Functions/Functions_Personal/js/script.js
+++ Week 4/Assignment_Functions/Functions_Personal/js/script.js
@@ -2,15 +2,22 @@
 
 //resturant meal 
 
-var meal = prompt ("How much do you want to spend on a meal, enter an amount between 10-20");//meal cost
-var tip = prompt (" Please enter a tip amount between 1-5");//tip amount
-var drink = 1.59;//drink cost
+var meal = parseInt(prompt ("How much do you want to spend on a meal, enter an amount between 10-20"));//meal cost
+var tip = parseInt(prompt (" Please enter a tip amount between 1-5"));//tip amount
+var drink = 2;//drink cost
 
-var total = totalPrice(meal, tip, drink);//argument 
+var total = totalPrice (meal, tip, drink);//argument 
 
 function totalPrice(meal, tip, drink){//parameters
 	var finalPrice = (meal + tip + drink);//calculation for total price
-	prompt("Your total price is $" + finalPrice);//prompt print out
+	return finalPrice;//return to top
 }
 
+(prompt ("Your total price is $" + total));//prompt print out
 
+
+var tipTotal = 14// check to see is your total bill is more than 14 dollars
+var finalTotal;
+
+finalTotal = (tipTotal< total)? "Have a nice night": "Your a cheap person";//Print out response
+prompt(finalTotal);
