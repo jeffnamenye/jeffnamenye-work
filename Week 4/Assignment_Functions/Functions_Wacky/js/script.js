6fd8f9c6f131c85864caf5e2f3b628a99d877bff
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-27 20:50:20 -0400

    test

--- Week 4/Assignment_Functions/Functions_Wacky/js/script.js
+++ Week 4/Assignment_Functions/Functions_Wacky/js/script.js
@@ -4,20 +4,20 @@
 //launch a satelite
 
 var satWeight = prompt ("Enter the a number between 1-250 for the weght of satelite");//prompt weight for satelite
-var thurstPower = prompt ("Enter a number between 1 and 300, for thust power");//thurst to lift offΩ;
+var thrustPower = prompt ("Enter a number between 1 and 300, for thust power");//thurst to lift offΩ;
 var booster = prompt ("Enter a number 1-100");//extra boost
 
 
-var thurstUp =   thrustPower - satWeight;
-var thrustBoost = thrustup + booster;
+var thrustUp =   thrustPower - satWeight;
+var thrustBoost = thrustUp + booster;
 
-if (thurstUp > 0){
+if (thrustUp > 0){
 	prompt ("You can lift off");
 
 }else if (thrustBoost > 0){
 	prompt ("You can lift off");
 
-}else { prompt ("You cant fly");
+}else { prompt ("You can not fly");
 
 }
 
