5912af382d83c9dadbdecf960ee7eb6bfac3a6d4
Jeff Namenye <jeffnamenye@hotmail.com>
2013-08-28 18:22:28 -0400

    first commit code

--- Week 4/Assignment_Functions/Functions_Personal/js/script.js
+++ Week 4/Assignment_Functions/Functions_Personal/js/script.js
@@ -1,2 +1,9 @@
 // Jeff Namenye 8/27/13 Functions_Personal
 
+//resturant meal 
+
+var meal = prompt ("How much do you want to spend on a meal, enter an amount between 10-20");
+var tip = prompt (" Please enter a tip amount between 1-5");
+var drink = 1.59;
+
+
